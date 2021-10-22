b60b899efb45802884d182cd177439ea59b6548f
logan Maloney <shakenasty13@gmail.com>
2021-10-22 14:02:49 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -11,7 +11,7 @@ namespace OperatorExercise
 
             int sum = a + b;
             int subtract = a - b;
-            int product = a * b; 
+            int product = a * b;
             int quotient = a / b;
             int remainder = a % b;
 
@@ -20,9 +20,28 @@ namespace OperatorExercise
             Console.WriteLine($"{a} * {b} is {product}");
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
 
+           
+            
+            
+            Console.WriteLine();
+            Console.WriteLine("what is the radius?");
+
+           // var radius = double.Parse(Console.ReadLine());
+
+          //  var radius = Convert.ToDouble(Console.ReadLine());
 
 
+            const double PI = 3.14; 
+            double radius; 
+            bool repeat = false;
+            do 
+            { 
+             repeat = double.TryParse(Console.ReadLine(), out radius);
+        } while (!repeat);
 
+            var areaOfCircle = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine($"the area of your circle is {areaOfCircle}.");
         }
     }
 }
